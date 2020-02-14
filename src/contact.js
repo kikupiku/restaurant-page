525c85a8373429cadaba191ab462002c0a743314
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-02-14 02:06:21 +0100

    create navbar, add event listeners, connect modules

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,14 @@
 let contactWrapper = document.createElement('div');
 contactWrapper.setAttribute('class', 'contact-wrapper');
 
+let title = document.createElement('h1');
+title.innerHTML = 'CONTACT US';
+title.setAttribute('class', 'section-title');
+contactWrapper.appendChild(title);
+
+let address = document.createElement('p');
+address.innerHTML = 'Hochenbrochenzockenallee 5 <br> 78954 Berlin <br> Telephone: 1357-1234567';
+address.setAttribute('class', 'address');
+contactWrapper.appendChild(address);
+
 export { contactWrapper };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { contactWrapper } from './contact.js';
 import { home } from './home.js';
 
 let content = document.getElementById('content');
+content.appendChild(home);
 
 let navBar = document.createElement('div');
 navBar.setAttribute('class', 'navbar');
-
 content.appendChild(navBar);
 
 let logo = document.createElement('img');
@@ -20,9 +20,46 @@ navBar.appendChild(name);
 
 let linkList = document.createElement('ul');
 linkList.setAttribute('class', 'link-list');
+navBar.appendChild(linkList);
 
-// content.appendChild(home);
+let createLink = () => {
+  let link;
 
-// content.appendChild(menuChoice);
+  for (let i = 0; i < 3; i++) {
+    link = document.createElement('li');
+    link.setAttribute('id', `link${i + 1}`);
+    if (i + 1 === 1) {
+      link.innerHTML = 'HOME';
+    } else if (i + 1 === 2) {
+      link.innerHTML = 'MENU';
+    } else if (i + 1 === 3) {
+      link.innerHTML = 'CONTACT';
+    }
 
-// content.appendChild(contactWrapper);
+    linkList.appendChild(link);
+
+    link.addEventListener('click', (event) => {
+      if (event.target.id === 'link1') {
+        content.appendChild(home);
+        content.appendChild(menuChoice);
+        content.appendChild(contactWrapper);
+        content.removeChild(menuChoice);
+        content.removeChild(contactWrapper);
+      } else if (event.target.id === 'link2') {
+        content.appendChild(home);
+        content.appendChild(menuChoice);
+        content.appendChild(contactWrapper);
+        content.removeChild(home);
+        content.removeChild(contactWrapper);
+      } else if (event.target.id === 'link3') {
+        content.appendChild(home);
+        content.appendChild(menuChoice);
+        content.appendChild(contactWrapper);
+        content.removeChild(home);
+        content.removeChild(menuChoice);
+      };
+    });
+  }
+};
+
+createLink();
